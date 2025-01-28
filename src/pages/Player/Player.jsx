35f246d5680afcc4d9a506a8f8af3f40681aec34
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-01-29 00:31:34 +0600

    player section done

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
-
+import back_arrow_icon from "../../assets/back_arrow_icon.png"
+import { key } from 'localforage'
+import { useParams } from 'react-router-dom'
 const Player = () => {
+  const[apiData,setApiData]=useState({
+    name:"",
+    key:"",
+    published_at:"",
+    typeof:""
+  })
+  const {id}=useParams()
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjQ0NGVkZDc2MGQ5M2I2MzVjMGNiMGIwZDdmNGEzNiIsIm5iZiI6MTczNzgwNzYzNy4xMTAwMDAxLCJzdWIiOiI2Nzk0ZDcxNTBlMWUwNDg2ZDYyYWQ4NzIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EKPIhD0Oe44xxpoUkX0Sw-EmR_qn4cNr9Mg3ERFhPQE'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  },[])
+  
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+       width="90%"
+       height="90%" 
+       src={ `https://www.youtube.com/embed/${apiData.key} `   }title='trailer'
+       frameBorder='0'
+       allowFullScreen
+       ></iframe>
+       <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.typeof}</p>
+       </div>
     </div>
   )
 }
